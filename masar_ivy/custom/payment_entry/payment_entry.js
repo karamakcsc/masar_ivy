c731c71eac2a7a98fbd6864689ed0dffa98670aa
karamakcsc <karamakcsc@hotmail.com>
2024-01-01 01:50:46 +0300

    siam

---
+++ masar_ivy/custom/payment_entry/payment_entry.js
@@ -0,0 +1,17 @@
+frappe.ui.form.on("Payment Entry","payment_type", function(frm) {
+    if (frm.doc.payment_type == "Receive") {
+      frm.set_value('naming_series', 'ACC-RV-.YYYY.-')
+      frm.refresh_fields();
+    }
+    else if (frm.doc.payment_type == "Pay"){
+      frm.set_value('naming_series', 'ACC-PAY-.YYYY.-')
+      frm.refresh_fields();
+    }
+    });
+
+    frappe.ui.form.on("Payment Entry",{ before_load:function(frm) {
+      var df=frappe.meta.get_docfield("Payment Entry", "naming_series",frm.doc.name);
+      df.read_only=1;
+    frm.refresh_fields();
+    }
+    });
--- masar_ivy/hooks.py
+++ masar_ivy/hooks.py
@@ -123,6 +123,9 @@ app_license = "MIT"
 #		"on_trash": "method"
 #	}
 # }
+doctype_js = {
+    "Payment Entry" : "custom/payment_entry/payment_entry.js"
+}
 
 # Scheduled Tasks
 # ---------------
