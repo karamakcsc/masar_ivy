9105a940fe4ea24510640485add504c0962123d7
karamakcsc <karamakcsc@hotmail.com>
2024-01-01 21:49:53 +0300

    SO Reservation

--- masar_ivy/custom/sales_order/sales_order.js
+++ masar_ivy/custom/sales_order/sales_order.js
@@ -217,3 +217,20 @@ function customizeReserveStockField(frm) {
     }
 }
 
+
+frappe.ui.form.on('Sales Order', {
+    onload: function (frm, cdt, cdn) {
+
+            cur_frm.fields_dict['items'].grid.wrapper.find('.btn-open-row').hide();
+            cur_frm.fields_dict['taxes'].grid.wrapper.find('.btn-open-row').hide();
+            frm.get_field('items').grid.cannot_add_rows = true;
+        
+    },
+    refresh: function (frm, cdt, cdn) {
+    
+            cur_frm.fields_dict['items'].grid.wrapper.find('.btn-open-row').hide();
+            cur_frm.fields_dict['taxes'].grid.wrapper.find('.btn-open-row').hide();
+            frm.get_field('items').grid.cannot_add_rows = true;
+        
+    }
+});
