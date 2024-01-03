2910278f820a8ff15b6381b4e77648c3a3b312bb
karamakcsc <karamakcsc@hotmail.com>
2024-01-03 12:41:20 +0300

    SINV

--- masar_ivy/custom/sales_invoice/sales_invoice.js
+++ masar_ivy/custom/sales_invoice/sales_invoice.js
@@ -1,5 +1,5 @@
 frappe.ui.form.on("Sales Invoice","onload", function(frm) {
-  if (!frappe.user.has_role('System Manager')) {
+  if (!frappe.user.has_role('Sales Manager')) {
         var df=frappe.meta.get_docfield("Sales Invoice", "update_stock",frm.doc.name);
         df.read_only=1;
       frm.set_value('update_stock', 1);
