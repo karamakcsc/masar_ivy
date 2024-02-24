dc701c0562822c9a7b204bdf57f0de262a25b39f
karamakcsc <karamakcsc@hotmail.com>
2024-02-25 02:01:30 +0300

    upsate

--- masar_ivy/custom/journal_entry/journal_entry.js
+++ masar_ivy/custom/journal_entry/journal_entry.js
@@ -1,6 +1,17 @@
-frappe.ui.form.on("Journal Entry","Eentry_type", function(frm) {
-    if (frm.doc.payment_type == "Credit Note"){
+frappe.ui.form.on("Journal Entry","voucher_type", function(frm) {
+    if (frm.doc.voucher_type == "Credit Note"){
       frm.set_value('naming_series', 'ACC-CN-.YYYY.-')
       frm.refresh_fields();
+    }
+    else {
+      frm.set_value('naming_series', 'ACC-JV-.YYYY.-')
+      frm.refresh_fields();
+    }
+    });
+
+    frappe.ui.form.on("Journal Entry",{ before_load:function(frm) {
+      var df=frappe.meta.get_docfield("Journal Entry", "naming_series",frm.doc.name);
+      df.read_only=1;
+    frm.refresh_fields();
     }
     });
