004bbeeb711bb2cd4af561c4bd9b9bd89f4c7693
karamakcsc <karamakcsc@hotmail.com>
2024-04-24 02:49:13 +0300

    SO

--- masar_ivy/custom/sales_order/sales_order.js
+++ masar_ivy/custom/sales_order/sales_order.js
@@ -34,40 +34,40 @@ frappe.ui.form.on("Sales Order", {
 });
 
 
-////////// mahmoud code to select item code 
-//////// filters selling price list 
+// ////////// mahmoud code to select item code 
+// //////// filters selling price list 
 
-frappe.ui.form.on('Sales Order', {
-    selling_price_list: function (frm) {
-        frm.set_query('item_code', 'items', function (doc, cdt, cdn) {
-            return {
-                query: 'masar_ivy.custom.sales_order.sales_order.select_item',
-                filters: {
-                    'selling_price_list': doc.selling_price_list
-                }
-            };
-        });
-    }
-});
+// frappe.ui.form.on('Sales Order', {
+//     selling_price_list: function (frm) {
+//         frm.set_query('item_code', 'items', function (doc, cdt, cdn) {
+//             return {
+//                 query: 'masar_ivy.custom.sales_order.sales_order.select_item',
+//                 filters: {
+//                     'selling_price_list': doc.selling_price_list
+//                 }
+//             };
+//         });
+//     }
+// });
 
-frappe.ui.form.on('Sales Order Item' , {
-    item_code :  function (frm, cdt, cdn){
-        var d = locals[cdt][cdn];
-        frappe.call({
-           method :  'masar_ivy.custom.sales_order.sales_order.check_item' , 
-           args :{
-            item_code :d.item_code, 
-            selling_price_list : frm.doc.selling_price_list
-           }, 
-           callback : function(r){
-            if (r.message === false) {
-                frappe.model.set_value(cdt, cdn, 'item_code', '');  
-            }
-           }
-        });
-    }
-});
-////////// mahmoud code to select item code 
-//////// filters selling price list 
+// frappe.ui.form.on('Sales Order Item' , {
+//     item_code :  function (frm, cdt, cdn){
+//         var d = locals[cdt][cdn];
+//         frappe.call({
+//            method :  'masar_ivy.custom.sales_order.sales_order.check_item' , 
+//            args :{
+//             item_code :d.item_code, 
+//             selling_price_list : frm.doc.selling_price_list
+//            }, 
+//            callback : function(r){
+//             if (r.message === false) {
+//                 frappe.model.set_value(cdt, cdn, 'item_code', '');  
+//             }
+//            }
+//         });
+//     }
+// });
+// ////////// mahmoud code to select item code 
+// //////// filters selling price list 
 
 
