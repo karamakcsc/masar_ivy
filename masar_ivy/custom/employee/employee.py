a513bacde7a11fc83d9aaa7a03a0c94fcbec4a7c
karamakcsc <karamakcsc@hotmail.com>
2024-07-01 15:06:04 +0300

    upload

--- masar_ivy/custom/employee/employee.py
+++ masar_ivy/custom/employee/employee.py
@@ -4,16 +4,16 @@ def validate(self , method):
 
 def add_ss_value(self):
     if float(self.custom_additional_social_security_salary) != 0:
-        contribution =  float(self.custom_additional_social_security_salary)
+        contribution = float(self.custom_additional_social_security_salary)
     else:
         contribution = float(self.custom_social_security_salary)
+
     comp_doc = frappe.get_doc('Company' , self.company)
     emp_comp_rate = comp_doc.custom_employee_share_rate
+
     if self.custom_employee_share_rate not in [0 , None]:
         rate = float(self.custom_employee_share_rate) / 100
     else:
-        rate  = float(emp_comp_rate) / 100
+        rate = float(emp_comp_rate) / 100
     self.custom_ss_component = contribution
     self.social_security_amount = contribution * rate 
-
-
