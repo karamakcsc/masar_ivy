88df7979a64fcb52701f0c75199884a42c3e8e65
karamakcsc <karamakcsc@hotmail.com>
2024-07-16 10:32:48 +0300

    update

--- masar_ivy/custom/journal_entry/journal_entry.js
+++ masar_ivy/custom/journal_entry/journal_entry.js
@@ -1,13 +1,13 @@
-frappe.ui.form.on("Journal Entry","voucher_type", function(frm) {
-    if (frm.doc.voucher_type == "Credit Note"){
-      frm.set_value('naming_series', 'ACC-CN-.YYYY.-')
-      frm.refresh_fields();
-    }
-    else {
-      frm.set_value('naming_series', 'ACC-JV-.YYYY.-')
-      frm.refresh_fields();
-    }
-    });
+// frappe.ui.form.on("Journal Entry","voucher_type", function(frm) {
+//     if (frm.doc.voucher_type == "Credit Note"){
+//       frm.set_value('naming_series', 'ACC-CN-.YYYY.-')
+//       frm.refresh_fields();
+//     }
+//     else {
+//       frm.set_value('naming_series', 'ACC-JV-.YYYY.-')
+//       frm.refresh_fields();
+//     }
+//     });
 
     // frappe.ui.form.on("Journal Entry",{ before_load:function(frm) {
     //   var df=frappe.meta.get_docfield("Journal Entry", "naming_series",frm.doc.name);
